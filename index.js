e1f9ae60cc5999f34d9d98430f42173882ff5fa8
解大劦 <9727005@qq.com>
2022-04-19 17:22:30 +0800

    feature: 初始化

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @file   : index.js
+ * @time   : 17:20
+ * @date   : 2022/4/19
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+function getVersion(){
+    return "0.0.1";
+}
